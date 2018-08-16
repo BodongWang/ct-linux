75715e5e815148cfa964b1594da9b44f73e02ebf
Eli Cohen <eli@mellanox.com>
2018-08-16 11:20:08 +0300

    net/mlx5: E-Switch, Re-read device caps after FDB create/destroy
    Re-read device capabilities after eswitch tables are created or
    destroyed to reflect capability changes that the firmware may apply, e.g
    due to tables created with encap flag set. It required for ECPF since it
    creates the FDB before the NIC netdevice is initialized. This change
    will allow it to read the correct capabilities.
    
    Issue: 1431255
    Change-Id: I23e4f63fc6e737ec106127873ad96bee500c120f
    Signed-off-by: Eli Cohen <eli@mellanox.com>

--- drivers/net/ethernet/mellanox/mlx5/core/eswitch_offloads.c
+++ drivers/net/ethernet/mellanox/mlx5/core/eswitch_offloads.c
@@ -1055,6 +1055,16 @@ int esw_offloads_init(struct mlx5_eswitch *esw, int nvfs)
 	if (err)
 		return err;
 
+	if (mlx5_core_is_ecpf(esw->dev)) {
+		/* read capabilities again since firmware may change
+		 * capabilities after creating tables with encap flags set. This
+		 * is required for ecpf since it switches to offloads mode
+		 * before the NIC drivers are initialized.
+		 */
+		mlx5_core_get_caps(esw->dev, MLX5_CAP_FLOW_TABLE);
+		mlx5_core_get_caps(esw->dev, MLX5_CAP_ETHERNET_OFFLOADS);
+	}
+
 	err = esw_create_offloads_table(esw);
 	if (err)
 		goto create_ft_err;
@@ -1099,6 +1109,14 @@ void esw_offloads_cleanup(struct mlx5_eswitch *esw, int nvfs)
 	esw_destroy_vport_rx_group(esw);
 	esw_destroy_offloads_table(esw);
 	esw_destroy_offloads_fdb_tables(esw);
+
+	if (mlx5_core_is_ecpf(esw->dev)) {
+		/* read capabilities again since firmware may change
+		 * capabilities after destroying tables with encap flags set
+		 */
+		mlx5_core_get_caps(esw->dev, MLX5_CAP_FLOW_TABLE);
+		mlx5_core_get_caps(esw->dev, MLX5_CAP_ETHERNET_OFFLOADS);
+	}
 }
 
 static int esw_mode_from_devlink(u16 mode, u16 *mlx5_mode)
