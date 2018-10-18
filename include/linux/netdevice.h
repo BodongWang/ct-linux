9b90f23469fb5915d0b1c830bf5484c48b76fff1
Yossi Kuperman <yossiku@mellanox.com>
2018-10-18 14:01:43 +0300

    WIP (memory issues: counters)
    Change-Id: I0a8aeb221eae423eac6f09f4c5834ee81c4a02b4

--- include/linux/netdevice.h
+++ include/linux/netdevice.h
@@ -800,12 +800,14 @@ enum tc_setup_type {
 struct tc_microflow_offload {
 	struct sk_buff *skb;
 	unsigned long cookie;
-	int last;
+	bool last_flow;
 };
 
 struct tc_ct_offload {
 	struct sk_buff *skb;
+	struct net *net;
 	struct nf_conntrack_tuple *tuple;
+	struct nf_conntrack_zone *zone;
 };
 
 /* These structures hold the attributes of bpf state that are being passed
